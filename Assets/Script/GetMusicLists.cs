ba72c88984c6080fb7d45224c4a9c533fbccd46d
cwxyz <jie844067636@gmail.com>
2017-03-19 11:47:50 +0800

    解决盘符选择

--- Assets/Script/GetMusicLists.cs
+++ Assets/Script/GetMusicLists.cs
@@ -2,43 +2,89 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetMusicLists : MonoBehaviour
 {
-    private AudioSource m_auio; 
+    private AudioSource m_auio;
+    public Dropdown dropdown;
     // Use this for initialization
     void Start()
     {
-        m_auio = gameObject.GetComponent<AudioSource>();
+        //m_auio = gameObject.GetComponent<AudioSource>();
 
-        string[] drives = Directory.GetLogicalDrives();
-        foreach (var item in drives)
-        {
-            string musicUrl = ReadDirsFiles(item);
+        //string[] drives = Directory.GetLogicalDrives();
+        //foreach (var item in drives)
+        //{
+        //    string musicUrl = ReadDirsFiles(item);
 
-            if (musicUrl != string.Empty)
-            {
-                Debug.Log(musicUrl);
+        //    if (musicUrl != string.Empty)
+        //    {
+        //        Debug.Log(musicUrl);
 
-                FileInfo file = new FileInfo(musicUrl);
-                WWW www = new WWW("file:///"+ file.FullName);
-                while (!www.isDone)
-                {
-                   // yield return 0;
-                }
-                
-                m_auio.clip = NAudioPlayer.FromMp3Data(www.bytes);
-                m_auio.Play();
-                Debug.Log(file.Name);
-            }
-        }
+        //        FileInfo file = new FileInfo(musicUrl);
+        //        WWW www = new WWW("file:///"+ file.FullName);
+        //        while (!www.isDone)
+        //        {
+        //           // yield return 0;
+        //        }
 
+        //        m_auio.clip = NAudioPlayer.FromMp3Data(www.bytes);
+        //        m_auio.Play();
+        //        Debug.Log(file.Name);
+        //    }
+        //}
+        InitDrivesData();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void InitDrivesData()
+    {
+        string[] drives = Directory.GetLogicalDrives();
+
+        dropdown.ClearOptions();
+        List<Dropdown.OptionData> list = new List<Dropdown.OptionData>();
+        {
+            Dropdown.OptionData data = new Dropdown.OptionData();
+            data.text = "All";
+            list.Add(data);
+        }
+        foreach (var item in drives)
+        {
+            Dropdown.OptionData data = new Dropdown.OptionData();
+            data.text = item;
+            list.Add(data);
+        }
+
+        dropdown.AddOptions(list);
+    }
+
+    public void AddMusicButtonClick()
+    {
+
+        dropdown.transform.parent.gameObject.SetActive(true);
+    }
+
+    public void DoneButtonClick()
+    {
+        string[] drives = Directory.GetLogicalDrives();
+        if (dropdown.value == 0)
+        {
+            foreach (var item in drives)
+            {
+                ReadDirsFiles(item);
+            }
+        }
+        else
+        {
+            ReadDirsFiles(drives[dropdown.value - 1]);
+        }
+        dropdown.transform.parent.gameObject.SetActive(false);
     }
 
     /// <summary>
@@ -52,13 +98,13 @@ public class GetMusicLists : MonoBehaviour
         {
             return resualt;
         }
+
         DirectoryInfo dirInfo = new DirectoryInfo(path);
         FileInfo[] files = dirInfo.GetFiles("*.mp3");
         foreach (var file in files)
         {
             Debug.Log(file.FullName);
             resualt = file.FullName;
-            return resualt;
         }
         DirectoryInfo[] dirs = dirInfo.GetDirectories();
         foreach (var item in dirs)
@@ -67,16 +113,12 @@ public class GetMusicLists : MonoBehaviour
             {
                 continue;
             }
-
-            string dirpath = Path.Combine(path, item.Name);
-            // Debug.Log(dirpath);
-
+            
             files = item.GetFiles("*.mp3");
             foreach (var file in files)
             {
                 Debug.Log(file.FullName);
                 resualt = file.FullName;
-                return resualt;
             }
         }
 
