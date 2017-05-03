83666f154ec0b5d9438ff9214540f18b8fe54701
cwxyz <jie844067636@gmail.com>
2017-05-03 14:52:18 +0800

    添加 readme 文件

--- Assets/Scripts/Test/ClickButtonAndShow.cs
+++ Assets/Scripts/Test/ClickButtonAndShow.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class ClickButtonAndShow : MonoBehaviour {
 
-    public GameObject m_button;
-
-    private AudioControl m_audioControl;
-
+    public GameObject m_clicked;
+    
     // Use this for initialization
     void Start () {
-        m_audioControl = new AudioControl();
+
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-        m_button = InputTools.GetMouseClickObj();
-        if (m_button != null)
+        m_clicked = InputTools.GetMouseClickObj();
+        if (m_clicked != null)
         {
             
         }
